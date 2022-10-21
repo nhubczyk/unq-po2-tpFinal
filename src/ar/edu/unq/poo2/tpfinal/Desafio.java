6ea29004bc6180fbd76a129b223c280817e239df
Lucio Jara <User@DESKTOP-NRJMSN6>
2022-10-20 21:42:25 -0300

    Se crea clase desafio y desafioUsuario para que compile

---
+++ src/ar/edu/unq/poo2/tpfinal/Desafio.java
@@ -0,0 +1,6 @@
+package ar.edu.unq.poo2.tpfinal;
+
+public class Desafio {
+	//se crea la clase para q compile usuario
+	private int cantidadMuestrasNecesarias;
+}
---
+++ src/ar/edu/unq/poo2/tpfinal/DesafioUsuario.java
@@ -0,0 +1,10 @@
+package ar.edu.unq.poo2.tpfinal;
+
+public class DesafioUsuario extends Desafio{
+
+	public float getPorcentajeCompletitud() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
