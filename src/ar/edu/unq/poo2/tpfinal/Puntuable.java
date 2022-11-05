fa463aa254f4b603f8aa87ddf653855d8b92c81c
ljara8 <luciojara8@gmail.com>
2022-11-04 21:20:04 -0300

    Se agrega la interfaz Puntuable, refactor Recomendaciones, test Recomendaciones

---
+++ src/ar/edu/unq/poo2/tpfinal/Puntuable.java
@@ -0,0 +1,9 @@
+package ar.edu.unq.poo2.tpfinal;
+
+public interface Puntuable {
+	public int getCantidadMuestras();
+
+	public int getDificultad();
+
+	public int getRecompensa();
+}
--- src/ar/edu/unq/poo2/tpfinal/RecomendacionFavoritos.java
+++ src/ar/edu/unq/poo2/tpfinal/RecomendacionFavoritos.java
@@ -4,32 +4,6 @@ import java.util.Comparator;
 import java.util.List;
 
 public class RecomendacionFavoritos extends Recomendacion {
-/*
-	@Override
-	public List<Desafio> desafiosRecomendadosSegunValor(Usuario usuario, Proyecto proyecto) {
-		return this.nDesafiosConMayorCoincidencia(20, proyecto.getDesafios(), usuario).stream().limit(5).toList();
-	}
-
-	@Override
-	public List<Desafio> nDesafiosConMayorCoincidencia(int cantidad, List<Desafio> desafiosNoAceptados,
-			Usuario usuario) {
-		List<Desafio> desafiosCoincidencia = desafiosNoAceptados.stream()
-				.sorted(Comparator.comparingInt(
-						d -> diferenciaEntreDesafios(d, usuario.getPreferenciasUsuario().getDesafioPreferido())))
-				.limit(cantidad).toList();
-		return desafiosCoincidencia;
-	}
-
-	public int diferenciaEntreDesafios(Desafio desafio, Desafio desafio2) {
-		return Math.abs(desafio.getDificultad() - desafio2.getDificultad())
-				+ Math.abs(desafio.getCantidadMuestrasRecolectadas() - desafio2.getCantidadMuestrasNecesarias())
-				+ Math.abs(desafio.getRecompensa() - desafio2.getRecompensa());
-	
-	}
-}
-*/
-
-
 
 	@Override
 	public List<Desafio> desafiosRecomendadosSegunValor(Usuario usuario, Proyecto proyecto) {
@@ -41,16 +15,9 @@ public class RecomendacionFavoritos extends Recomendacion {
 			Usuario usuario) {
 		List<Desafio> desafiosCoincidencia = desafiosNoAceptados.stream()
 				.sorted(Comparator.comparingInt(
-						d -> diferenciaEntreDesafios(d, usuario.getPreferenciasUsuario().getDesafioPreferido())))
+						d -> d.similitudConDesafio(usuario.getPreferenciasUsuario().getDesafioPreferido())))
 				.limit(cantidad).toList();
 		return desafiosCoincidencia;
 	}
 
-	public int diferenciaEntreDesafios(Desafio desafio, Desafio desafio2) {
-		return Math.abs(desafio.getDificultad() - desafio2.getDificultad())
-				+ Math.abs(desafio.getCantidadMuestrasRecolectadas() - desafio2.getCantidadMuestrasNecesarias())
-				+ Math.abs(desafio.getRecompensa() - desafio2.getRecompensa());
-	}
-
 }
- 
